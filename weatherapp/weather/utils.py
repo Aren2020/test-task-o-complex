4f5f88c5f55bcec800b4ca7116bc50a224aeac44
Aren2020 <aren.allahverdyan@gmail.com>
2024-07-21 16:45:45 +0400

    set private weather class methods

--- weatherapp/weather/utils.py
+++ weatherapp/weather/utils.py
@@ -9,7 +9,7 @@ r = redis.StrictRedis(host = settings.REDIS_HOST,
 
 class Weather:
 
-    def get_lat_long(self, city):
+    def _get_lat_long(self, city):
         geolocator = Nominatim(user_agent = "Weather App")
         location = geolocator.geocode(city)
         if location:
@@ -18,7 +18,7 @@ class Weather:
             return None, None
 
     def get_weather(self, city):
-        lat, long = self.get_lat_long(city)
+        lat, long = self._get_lat_long(city)
         params = {
             'latitude': lat,
             'longitude': long,
@@ -30,23 +30,23 @@ class Weather:
         url = 'https://api.open-meteo.com/v1/forecast'
         response = requests.get(url, params = params)
         try:
-            response = self.data_format( response.json() )
+            response = self._data_format( response.json() )
             r.incr(f'cities:{city}:views')
         except json.JSONDecodeError:
             response = 'That city is not available in our application'
         return response
 
-    def get_day_of_week(self, date):
+    def _get_day_of_week(self, date):
         date_obj = datetime.fromisoformat(date)
         return date_obj.strftime("%A")
 
 
-    def data_format(self, data):
+    def _data_format(self, data):
         response = {}
         date_list = data['hourly']['time']
         for i, date_str in enumerate(date_list):
             date, time = date_str.split('T')
-            day_of_week = self.get_day_of_week(date_str)
+            day_of_week = self._get_day_of_week(date_str)
             
             part = {time: {
                         'wind_speed_10m': f'{ data["hourly"]["wind_speed_10m"][i] } km/h',
